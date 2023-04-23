baf4f1052d391a822445f16597192a508e23eb38
Karin Kikas <karin.kikas@gmail.com>
2023-04-23 22:22:10 +0300

    Rename columns and functions. Add typehints and printouts. Not finished

--- homework_2_databases_kkikas/plain_sql/sql_connect.py
+++ homework_2_databases_kkikas/plain_sql/sql_connect.py
@@ -21,6 +21,7 @@ def open_connection(database: str) -> None:
     """
     Create database connection.
     """
+
     global CONNECTION
     CONNECTION = sqlite3.connect(database)
     print("Database opened successfully")
@@ -32,7 +33,7 @@ def create_provider() -> None:
     """
     CONNECTION.execute("""CREATE TABLE IF NOT EXISTS PROVIDER
         (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        PROVIDERNAME TEXT UNIQUE NOT NULL);""")
+        ProviderName TEXT UNIQUE NOT NULL);""")
     print("Table PROVIDER successfully created.")
 
 
@@ -42,97 +43,51 @@ def create_canteen() -> None:
     """
     CONNECTION.execute("""CREATE TABLE IF NOT EXISTS CANTEEN
         (ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-        PROVIDERID INT NOT NULL,
-        NAME TEXT NOT NULL,
-        LOCATION TEXT,
-        TIME_OPEN INT,
-        TIME_CLOSED INT,
-        FOREIGN KEY (PROVIDERID) REFERENCES PROVIDER (ID)
+        ProviderID INT NOT NULL,
+        Name TEXT NOT NULL,
+        Location TEXT,
+        time_open SMALLINT NOT NULL,
+        time_closed SMALLINT NOT NULL,
+        FOREIGN KEY (ProviderID) REFERENCES PROVIDER (ID)
         ON UPDATE CASCADE
         ON DELETE SET NULL);""")
     print("Table CANTEEN successfully created.")
 
 
-def create_it_college_record():
+def insert_one_record(rec: dict) -> None:
     """
     Create records in CANTEEN and PROVIDER tables
     according to the canteen located in IT-College.
     """
     CONNECTION.execute("""PRAGMA foreign_keys = ON;""")
     CONNECTION.execute("""INSERT INTO PROVIDER\
-                       (PROVIDERNAME)\
+                       (ProviderName)\
                        VALUES\
-                       ('Bitt OÜ')""")
+                       (:provider_name)""", (rec["ProviderName"],))
     CONNECTION.execute("""INSERT INTO CANTEEN\
-                       (PROVIDERID, NAME, LOCATION,\
-                       TIME_OPEN, TIME_CLOSED)\
+                       (ProviderID, Name, Location,\
+                       time_open, time_closed)\
                        VALUES\
-                       ((SELECT ID FROM PROVIDER WHERE PROVIDERNAME='Bitt OÜ'),\
-                        'bitStop KOHVIK', 'IT College, Raja 4c',\
-                       930, 1600)""")
+                       ((SELECT ID FROM PROVIDER \
+                        WHERE ProviderName=:provider_name),\
+                            :name, :location,\
+                                :open, :closed)""", (
+        rec["ProviderName"], rec["Name"], rec["Location"],
+        rec["time_open"], rec["time_closed"]
+    ))
     CONNECTION.commit()
     print("IT College record created.")
 
 
-def create_records_in_bulk():
+def insert_records_in_bulk(diners: list) -> None:
     """
     Create records in CANTEEN and PROVIDER tables
     for canteens other than the one located in IT-College.
     """
 
-    canteens = [
-        ['Rahva Toit',
-         "Economics- and social science building canteen",
-         "Akadeemia tee 3, SOC- building",
-         830,
-         1830],
-        ['Rahva Toit',
-         "Library canteen",
-         "Akadeemia tee 1/Ehitajate tee 7",
-         830,
-         1900
-         ],
-        ['Baltic Restaurants Estonia AS',
-         "Main building Deli cafe",
-         "Ehitajate tee 5, U01 building",
-         900,
-         1630
-         ],
-        ['Baltic Restaurants Estonia AS',
-         "Main building Daily lunch restaurant",
-         "Ehitajate tee 5, U01 building",
-         900,
-         1630
-         ],
-        ['Rahva toit',
-         "U06 building canteen",
-         "NULL",
-         900,
-         1600
-         ],
-        ['Baltic Restaurants Estonia AS',
-         "Natural Science building canteen",
-         "Akadeemia tee 15, SCI building",
-         900,
-         1600
-         ],
-        ['Baltic Restaurants Estonia AS',
-         "ICT building canteen",
-         "Raja 15/Mäepealse 1",
-         900,
-         1600
-         ],
-        ['TTÜ Sport OÜ',
-         "Sports building canteen",
-         "Männiliiva 7, S01 building",
-         1100,
-         2000
-         ]
-    ]
-
     unique_providers = {}
-    providers = [provider[0]
-                 for provider in canteens]
+    providers = [provider["ProviderName"]
+                 for provider in diners]
 
     for name in providers:
         if name.casefold() not in unique_providers:
@@ -145,63 +100,80 @@ def create_records_in_bulk():
 
     cursor = CONNECTION.cursor()
     cursor.executemany(
-        """INSERT OR REPLACE INTO PROVIDER (PROVIDERNAME) VALUES(:name);""", providers)
+        """INSERT OR REPLACE INTO PROVIDER (ProviderName) VALUES(:name);""", providers)
 
-    for index in range(len(canteens)):
-        provider_name = (canteens[index][0],)
+    for i, element in enumerate(diners):
+        provider_name = (element["ProviderName"],)
         result = cursor.execute(
-            "SELECT ID FROM PROVIDER WHERE PROVIDERNAME=? COLLATE NOCASE;", provider_name)
+            "SELECT ID FROM PROVIDER WHERE ProviderName=? COLLATE NOCASE;", provider_name)
         res = [row[0] for row in result][0]
-        canteens[index][0] = res
+        # Replace provider name with id.
+        element["ProviderName"] = res
 
     CONNECTION.executemany(
-        """INSERT INTO CANTEEN (PROVIDERID, NAME, LOCATION, TIME_OPEN, TIME_CLOSED) \
-            VALUES (?, ?, ?, ?, ?);""", canteens)
+        """INSERT INTO CANTEEN (ProviderID, Name, Location, time_open, time_closed) \
+            VALUES (?, ?, ?, ?, ?);""", [tuple(diner.values()) for diner in diners])
     CONNECTION.commit()
     print("The rest of the records created.")
 
 
-def query_records():
+def query_records() -> list:
     """
     Query records from database tables.
     """
     cursor = CONNECTION.execute("SELECT * FROM CANTEEN;")
     CONNECTION.commit()
     for row in cursor:
-        print("ID ", row[0])
-        print("PROVIDERID ", row[1])
-        print("NAME ", row[2])
-        print("LOCATION ", row[3])
-        print("TIME_OPEN ", row[4])
-        print("TIME_CLOSED ", row[5])
+        print("ID: ", row[0])
+        print("ProviderID: ", row[1])
+        print("Name: ", row[2])
+        print("Location: ", row[3])
+        print("time_open: ", row[4])
+        print("time_closed: ", row[5])
+        print(".  ." * 10)
     print("End of query.")
+    return cursor
 
 
-def query_open_900_1620():
+def query_open_between_inclusive(time_open: int, time_closed: int) -> list:
     """
     Query records for canteens which are open from 09.00 AM to 16.20 PM.
     """
     cursor = CONNECTION.cursor()
     cursor.execute(
-        """SELECT NAME, TIME_OPEN, TIME_CLOSED FROM CANTEEN WHERE TIME_OPEN <= 900 AND TIME_CLOSED >= 1620;""")
+        """SELECT Name, time_open, time_closed FROM CANTEEN WHERE \
+            time_open <= :time_open AND time_closed >= :time_closed;""",
+        (time_open, time_closed))
     CONNECTION.commit()
-    print("Canteens open from 09.00 AM until 16.20 PM:")
+    print("Canteens open from ", end="")
+    print(f"{str(time_open)[:-2]}.{str(time_open)[-2:]} AM ", end="")
+    print(f"until {str(time_closed)[:-2]}.{str(time_closed)[-2:]} PM:\n")
     for row in cursor:
-        print(
-            f"{row[0]} is open from {str(row[1])[:-2]}.{str(row[1])[-2:]} AM until {str(row[2])[:-2]}.{str(row[2])[-2:]} PM")
+        print(f"{row[0]} is open")
+        print(f"from {str(row[1])[:-2]}.{str(row[1])[-2:]} AM")
+        print(f"until {str(row[2])[:-2]}.{str(row[2])[-2:]} PM")
+        print(".  ." * 10)
     print("End of query.")
+    return list(cursor)
 
 
-def query_canteens_serviced_by():
-    provider = 'Baltic Restaurants Estonia AS'
+def query_canteens_serviced_by(provider: str) -> str:
+    """
+    Query canteens serviced by provider given as and argument.
+    """
     cursor = CONNECTION.cursor()
     cursor.execute(
-        """SELECT NAME FROM CANTEEN LEFT JOIN PROVIDER ON PROVIDER.ID=CANTEEN.PROVIDERID WHERE PROVIDERNAME=?;""", (provider,))
+        """SELECT NAME FROM CANTEEN LEFT JOIN PROVIDER ON \
+            PROVIDER.ID=CANTEEN.ProviderID WHERE ProviderName=?;""", (provider,))
     CONNECTION.commit()
-    print("Canteens serviced by Baltic Restaurants Estonia AS are:")
+    print(f"Canteens serviced by {provider} are:")
+    result = ""
     for row in cursor:
+        result = row[0]
         print(row[0])
+        print(".  ." * 10)
     print("End of query.")
+    return result
 
 
 def delete_canteen() -> None:
@@ -209,7 +181,7 @@ def delete_canteen() -> None:
     Delete CANTEEN table from DINERS database.
     """
     CONNECTION.execute("""DROP TABLE CANTEEN""")
-    print("Table CANTEEN successfully deleted.")
+    print("Table CANTEEN deleted successfully.")
 
 
 def delete_provider() -> None:
@@ -217,7 +189,7 @@ def delete_provider() -> None:
     Delete PROVIDER table from DINERS database.
     """
     CONNECTION.execute("""DROP TABLE PROVIDER""")
-    print("Table PROVIDER successfully deleted.")
+    print("Table PROVIDER deleted successfully.")
 
 
 def close_connection() -> None:
@@ -225,18 +197,95 @@ def close_connection() -> None:
     Close database connection.
     """
     CONNECTION.close()
-    print("Database connection closed successfully")
+    print("Database connection successfully closed.")
 
 
 if __name__ == "__main__":
+    record = {
+        "ProviderName": "Bitt OÜ",
+        "Name": "bitStop KOHVIK",
+        "Location": "IT College, Raja 4c",
+        "time_open": 900,
+        "time_closed": 1600
+    }
+    canteens = [
+        {
+            "ProviderName": "Rahva Toit",
+            "Name": "Economics- and social science building canteen",
+            "Location": "Akadeemia tee 3, SOC- building",
+            "time_open": 830,
+            "time_closed": 1830
+        },
+        {
+            "ProviderName": "Rahva Toit",
+            "Name": "Library canteen",
+            "Location": "Akadeemia tee 1/Ehitajate tee 7",
+            "time_open": 830,
+            "time_closed": 1900
+        },
+        {
+            "ProviderName": "Baltic Restaurants Estonia AS",
+            "Name": "Main building Deli cafe",
+            "location": "Ehitajate tee 5, U01 building",
+            "time_open": 900,
+            "time_closed": 1630
+        },
+        {
+            "ProviderName": "Baltic Restaurants Estonia AS",
+            "Name": "Main building Daily lunch restaurant",
+            "Location": "Ehitajate tee 5, U01 building",
+            "time_open": 900,
+            "time_closed": 1630
+        },
+        {
+            "ProviderName": "Rahva toit",
+            "Name": "U06 building canteen",
+            "Location": "",
+            "time_open": 900,
+            "time_closed": 1600
+        },
+        {
+            "ProviderName": "Baltic Restaurants Estonia AS",
+            "Name": "Natural Science building canteen",
+            "Location": "Akadeemia tee 15, SCI building",
+            "time_open": 900,
+            "time_closed": 1600
+        },
+        {
+            "ProviderName": "Baltic Restaurants Estonia AS",
+            "Name": "ICT building canteen",
+            "Location": "Raja 15/Mäepealse 1",
+            "time_open": 900,
+            "time_closed": 1600
+        },
+        {
+            "ProviderName": "TTÜ Sport OÜ",
+            "Name": "Sports building canteen",
+            "Location": "Männiliiva 7, S01 building",
+            "time_open": 1100,
+            "time_closed": 2000
+        }
+    ]
+
     open_connection("DINERS.db")
+    print("--*--" * 10)
     delete_canteen()
     delete_provider()
+    print("--*--" * 10)
     create_provider()
+    print("--*--" * 10)
     create_canteen()
-    create_it_college_record()
-    create_records_in_bulk()
+    print("--*--" * 10)
+    insert_one_record(record)
+    print("--*--" * 10)
+    insert_records_in_bulk(canteens)
+    print("--*--" * 10)
     query_records()
-    query_open_900_1620()
-    query_canteens_serviced_by()
+    print("--*--" * 10)
+    query_open_between_inclusive(900, 1620)
+    print("--*--" * 10)
+    query_canteens_serviced_by("Baltic Restaurants Estonia AS")
+    print("--*--" * 10)
     close_connection()
+    print("X" * 40)
+    print("*" * 40)
